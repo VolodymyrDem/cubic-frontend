779c0cf09b09266fc5590a76bcc46499a402d761
Volodymyr Demianchuk <dem.volodymyrrr@gmail.com>
2025-12-28 18:38:27 +0200

    Update URLs for VolodymyrDem GitHub Pages

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__APP_CONFIG__ = {
   APP_BASE_PATH: "/cubic-frontend", // Ваша назва репо на GitHub Pages
   API_BASE_URL: "http://localhost:8000", // Змініть на production API URL
   GOOGLE_CLIENT_ID: "156322397230-dloj26s348rut4u70e8r6h6680m0abjh.apps.googleusercontent.com",
-  GOOGLE_REDIRECT_URI: "https://poliukhovych.github.io/cubic-frontend/auth/callback", // GitHub Pages URL
+  GOOGLE_REDIRECT_URI: "https://volodymyrdem.github.io/cubic-frontend/auth/callback", // GitHub Pages URL
   GOOGLE_USE_CODE_FLOW: "1",
   DEV_AUTH: "1", // Встановіть "0" для production
   ADMIN_USERNAME: "admin",
